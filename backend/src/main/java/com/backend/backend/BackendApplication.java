2797d01448f0ef60488ddce2e2fe0f590f35993b
Harshit <harshitdcharan@gmail.com>
2025-10-26 13:47:33 +0530

    changed from gradle to maven

--- backend/src/main/java/com/backend/backend/BackendApplication.java
+++ backend/src/main/java/com/backend/backend/BackendApplication.java
@@ -1,4 +1,4 @@
-package com.queryx.backend;
+package com.backend.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- backend/src/test/java/com/backend/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/backend/backend/BackendApplicationTests.java
@@ -1,4 +1,4 @@
-package com.queryx.backend;
+package com.backend.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
